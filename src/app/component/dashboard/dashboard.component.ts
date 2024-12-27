3f3fd5d36f1cc1ff3b7c1d982dc27dfac077b17f
Aislei Ávila de Medeiros <aislei@outlook.com.br>
2024-12-27 22:19:23 +0000

    Diversos Ajustes

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -7,27 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   functionalities = [
-    { name: 'Home', route: '/home', image: 'assets/icons/home.png' },
+    { name: 'Home', route: '/home', image: 'assets/icons/home.svg' },
     {
       name: 'Produtos',
       route: '/produtos',
-      image: 'assets/icons/produtos.png',
+      image: 'assets/icons/inventory_2.svg',
     },
     {
       name: 'Unidades Federativas',
       route: '/unidades-federativas',
-      image: 'assets/icons/unidades-federativas.png',
+      image: 'assets/icons/location_city.svg',
     },
     {
       name: 'Usuários',
       route: '/usuarios',
-      image: 'assets/icons/usuarios.png',
+      image: 'assets/icons/person.svg',
     },
-    { name: 'Chat', route: '/chat', image: 'assets/icons/chat.png' },
+    { name: 'Chat', route: '/chat', image: 'assets/icons/chat.svg' },
     {
       name: 'Organização',
       route: '/organizacao',
-      image: 'assets/icons/organizacao.png',
+      image: 'assets/icons/business.svg',
     },
     // ...additional functionalities...
   ];
