41b50ee9256c0c2f68597e42c8a7088375dadf7f
Aislei Ávila de Medeiros <aislei@outlook.com.br>
2024-12-29 16:27:16 +0000

    Migração para o Angualr 19

--- src/app/component/login/login.component.spec.ts
+++ src/app/component/login/login.component.spec.ts
@@ -1,7 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
-// import { HeaderModule } from '../header/header.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
