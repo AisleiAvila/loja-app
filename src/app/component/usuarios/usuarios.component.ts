8c3e6205bb09a4c1dbad392f2a5fd1913abcb3f5
Aislei Ávila de Medeiros <aislei@outlook.com.br>
2025-02-22 21:10:46 +0000

    Ajuste tabela endereco

--- src/app/component/usuarios/usuarios.component.ts
+++ src/app/component/usuarios/usuarios.component.ts
@@ -250,7 +250,6 @@ export class UsuariosComponent implements AfterViewInit, OnInit {
   }
 
   excluirUsuario(id: number): void {
-    alert('Excluir usuário com ID: ' + id);
     this.usuariosService.deleteUsuario({ id }).subscribe(
       () => {
         this.snackBar.open('Usuário excluído com sucesso!', 'Fechar', {
